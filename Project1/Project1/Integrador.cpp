bb14bf0cb308f71c25a82b72ca84a803d240e8ff
bottzo <45202057+bottzo@users.noreply.github.com>
2019-11-15 12:18:57 +0100

    aver ara

---
+++ Project1/Project1/Integrador.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+void Euler_Integrator(float &pos,float &vel,float &a,float &dt) {
+	pos += (vel* dt);
+	vel += a* dt;
+}
+
+int main() {
+	float pos = 0; float vel = 5; float a = 0; float dt = (1.0f / 30.0f);
+	Euler_Integrator(pos,vel,a,dt);
+	cout << pos << endl << vel << endl << a<<endl;
+	Euler_Integrator(pos,vel,a,dt);
+	cout << pos << endl << vel << endl << a;
+	system("pause");
+}

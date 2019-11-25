ec39970d8393e7647f3a07aa84505d6cb69e08c3
bottzo <45202057+bottzo@users.noreply.github.com>
2019-11-25 18:32:28 +0100

    gravetat negativa
    + demostrar el puja i baixa si el framerate =1

--- Project1/Project1/Integrador.cpp
+++ Project1/Project1/Integrador.cpp
@@ -23,7 +23,7 @@ int main()
 	//For the case an object moves in the Y-axis
 	float posy = 0;
 	float vely = 5;
-	float g = 10;
+	float g = -10;
 
 	//For the case a drag force is existent ( a cannon ball with 10kg of mass, 2m^2 of area)
 	//cannonball case
@@ -49,39 +49,49 @@ int main()
 	
 	cout << "||   Euler Integrator   ||" << endl << endl;
 
-	Euler_Integrator(posx, velx, ax, dt);
-	cout << "position: " << posx << endl;
-	cout << "velocity: " << velx << endl;
+	cout << "Initial position: " << posx << endl;
+	cout << "Initial velocity: " << velx << endl;
 	cout << "Constant acceleration: " << ax << endl<<endl;
 	Euler_Integrator(posx, velx, ax, dt);
 	cout << "position next frame: " << posx << endl;
 	cout << "velocity next frame: " << velx << endl;
 	cout << "Constant acceleration: " << ax << endl << endl;
 	cout << "--------------------------------------------------------" << endl;
+
 	cout << "Case of a falling object" << endl;
-	Euler_Integrator(posy, vely, g, dt);
-	cout << "position Y: " << posy << endl;
-	cout << "velocity Y: " << vely << endl;
-	cout << "constant gravity: " << g << endl << endl;
+	cout << "Initial position Y: " << posy << endl;
+	cout << "Initial velocity Y: " << vely << endl;
+	cout << "Constant gravity: " << g << endl << endl;
 	Euler_Integrator(posy, vely, g, dt);
 	cout << "position Y next frame: " << posy << endl;
 	cout << "velocity Y next frame: " << vely << endl;
-	cout << "constant gravity: " << g << endl;
+	cout << "constant gravity: " << g << endl<<endl;
+	if (framerate == 1) {
+		Euler_Integrator(posy, vely, g, dt);
+		cout << "position Y next frame: " << posy << endl;
+		cout << "velocity Y next frame: " << vely << endl;
+		cout << "constant gravity: " << g << endl<<endl;
+		Euler_Integrator(posy, vely, g, dt);
+		cout << "position Y next frame: " << posy << endl;
+		cout << "velocity Y next frame: " << vely << endl;
+		cout << "constant gravity: " << g << endl;
+	}
 	cout << "--------------------------------------------------------" << endl;
+
 	cout << "Case of an object moving with adverse drag force" << endl << endl;
+
 	cout << "This is the case of a cannonball with an area of 2m^2, a mass of 10kg and the same acceleration of case 1 (8m/s^2)" << endl;
-	Euler_Integrator(posc, velc, ac, dt);
-	cout << "position with drag: " << posc << endl;
-	cout << "velocity with drag: " << velc << endl;
+	cout << "Initial position with drag: " << posc << endl;
+	cout << "Initial velocity with drag: " << velc << endl;
 	cout << "acceleration with drag: " << ac << endl << endl;
 	Euler_Integrator(posc, velc, ac, dt);
 	cout << "position of a cannonball with drag next frame: " << posc << endl;
 	cout << "velocity of a cannonball with drag next frame: " << velc << endl;
 	cout << "acceleration of a cannonball with drag: " << ac << endl << endl;
+
 	cout << "This is the case of a bullet with an area of 0,0025m^2, a mass of 0,1kg and an acceleration of 20m/s^2" << endl;
-	Euler_Integrator(posb, velb, ab, dt);
-	cout << "position of a bullet with drag: " << posb << endl;
-	cout << "velocity of a bullet with drag: " << velb << endl;
+	cout << "Initial position of a bullet with drag: " << posb << endl;
+	cout << "Initial velocity of a bullet with drag: " << velb << endl;
 	cout << "acceleration of a bullet with drag: " << ab << endl << endl;
 	Euler_Integrator(posb, velb, ab, dt);
 	cout << "position of a bullet with drag next frame: " << posb << endl;
